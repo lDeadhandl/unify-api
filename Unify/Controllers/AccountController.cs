a130261596435cac5e65fc56db04eeed97e5c704
Stefan Vladetic <stefanvladetic@a-yr246lvcg.carfax.cfx>
2020-02-25 10:39:22 -0500

    Firestore init/add data

--- Unify/Controllers/AccountController.cs
+++ Unify/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Google.Cloud.Firestore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -22,6 +23,8 @@ namespace Unify.Controllers
             _spotifyService = spotifyService;
         }
 
+
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
@@ -32,20 +35,42 @@ namespace Unify.Controllers
         [HttpGet("{userId}/tracks")]
         public async Task<ActionResult<List<string>>> GetTracks(string userId, [FromQuery] int x)
         {
-            var user = await _unifyContext.User.FindAsync(userId);
+            var db = FirestoreDb.Create("unify-database");
 
-            // Set the authorization
-            var userService = await _spotifyService.GetUserService(user.SpotifyAccessToken); // ADD: error handling for tokens
+            var snapshot = await db.Collection("Users").Document(userId).GetSnapshotAsync();
+
+            var SAT = snapshot.ToDictionary()["SpotifyAccessToken"];
+
+            //// Set the authorization
+            var userService = await _spotifyService.GetUserService(SAT.ToString()); // ADD: error handling for tokens
 
             var tracks = await userService.GetUserTracks(x);
 
             return Ok(tracks);
         }
 
-        
+        // POST 
+        [HttpPost("add")]
+        public async Task<ActionResult> AddUsers(string project)
+        {
+            var db = FirestoreDb.Create(project);
+            DocumentReference docRef = db.Collection("users").Document("al");
+            Dictionary<string, object> user = new Dictionary<string, object>
+            {
+                { "First", "ASDFASDF" },
+                { "Last", "WAAAAAAAAAAAA" },
+                { "Born", 1815 }
+            };
+            await docRef.SetAsync(user);
+
+            return Ok();
+        }
+
         [HttpPost]
         public async Task<ActionResult> Authorize(string token)
         {
+            var db = FirestoreDb.Create("unify-database");
+
             // Authenticate the user
             var authentication = await _spotifyService.Authenticate(token);
 
@@ -54,27 +79,35 @@ namespace Unify.Controllers
 
             var profile = await userService.GetUserProfile();
 
-            // Check if user exists 
-            var userExists = await _unifyContext.User.AnyAsync(u => u.Id == profile.Id);
+            var docRef = db.Collection("Users").Document($"{profile.Id}");
 
-            if (userExists){
-            // If user exists, update tokens
-                var User = await _unifyContext.User.FindAsync(profile.Id);
-                User.SpotifyAccessToken = authentication.AccessToken;
-                User.SpotifyRefreshToken = authentication.RefreshToken;
-                _unifyContext.SaveChanges();
-            }
-            else{
-            // Add new user with tokens
-                _unifyContext.Add(new User
-                {
-                    DisplayName = profile.DisplayName,
-                    Id = profile.Id,
-                    SpotifyAccessToken = authentication.AccessToken,
-                    SpotifyRefreshToken = authentication.RefreshToken
-                });
-                _unifyContext.SaveChanges();
-            }
+            var User = new User() {
+                DisplayName = profile.DisplayName,
+                SpotifyAccessToken = authentication.AccessToken,
+                SpotifyRefreshToken = authentication.RefreshToken
+            };
+            await docRef.SetAsync(User);
+            //// Check if user exists 
+            //var userExists = await _unifyContext.User.AnyAsync(u => u.Id == profile.Id);
+
+            //if (userExists){
+            //// If user exists, update tokens
+            //    var User = await _unifyContext.User.FindAsync(profile.Id);
+            //    User.SpotifyAccessToken = authentication.AccessToken;
+            //    User.SpotifyRefreshToken = authentication.RefreshToken;
+            //    _unifyContext.SaveChanges();
+            //}
+            //else{
+            //// Add new user with tokens
+            //    _unifyContext.Add(new User
+            //    {
+            //        DisplayName = profile.DisplayName,
+            //        Id = profile.Id,
+            //        SpotifyAccessToken = authentication.AccessToken,
+            //        SpotifyRefreshToken = authentication.RefreshToken
+            //    });
+            //    _unifyContext.SaveChanges();
+            //}
 
             // Return something that is unique to the user and that the backend can use to retrieve their data
             return Ok(profile);
--- Unify/Data/User.cs
+++ Unify/Data/User.cs
@@ -1,30 +1,40 @@
 ﻿using System.Collections.Generic;
+using Google.Cloud.Firestore;
 
 namespace Unify.Data
 {
+    [FirestoreData]
     public class User
     {
+        [FirestoreProperty]
         public string Id { get; set; }      // By convention, a property named Id or <type name>Id will be configured as the key of an entity.
 
+        [FirestoreProperty]
         public string DisplayName { get; set; }
 
+        [FirestoreProperty]
         public string Email { get; set; }
         /// <summary>
         /// Subcription level (premium, free or open)
         /// </summary>
+        [FirestoreProperty]
         public string Product { get; set; }
         /// <summary>
         /// The resource identifier that you can enter, for example, in the Spotify Desktop client’s search box to locate the user
         /// </summary>
+        [FirestoreProperty]
         public string Uri { get; set; }
 
         /// <summary>
         /// Used to get a new auth token if the current one is expired
         /// </summary>
+        [FirestoreProperty]
         public string SpotifyRefreshToken { get; set; }
 
+        [FirestoreProperty]
         public string SpotifyAccessToken { get; set; }
 
+        [FirestoreProperty]
         public List<Party> Parties { get; set; }
     }
 }
